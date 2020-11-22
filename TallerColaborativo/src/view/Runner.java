8f09153b0f613cce9207841c91ae024d465e270e
yeison <jyeison@yeison>
2020-11-22 14:19:47 -0500

    pruebas

--- TallerColaborativo/src/view/Runner.java
+++ TallerColaborativo/src/view/Runner.java
@@ -8,12 +8,12 @@ public class Runner {
 		String palabra = "sogamoso ciudad del sol y del acero";
 		System.out.println("Entrada :"+palabra);
 		Management obj = new Management(palabra);
-		System.out.println("Mayusculas :"+obj.ownName());
-		String b = Management.encrypt(palabra);
-		System.out.println("Encriptada : "+b);
-		System.out.println("Deencriptada : "+Management.decrypt(b));
-		System.out.println("Interseccion : "+obj.intersection("colombia"));
-		System.out.println("Diferencia : "+obj.difference("colombia"));
+		System.out.println("Mayusculas :"+ obj.ownName());
+		String b = Management.encrypt(obj.ownName());
+		System.out.println("Encriptada :"+b);
+		System.out.println("Deencriptada :"+Management.decrypt(b));
+		System.out.println("Interseccion :"+obj.intersection("colombia"));
+		System.out.println("Diferencia :"+obj.difference("colombia"));
 	}
 
 }
