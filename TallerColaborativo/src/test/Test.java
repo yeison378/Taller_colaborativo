03329eaec1bd73e6e9f628f8db802f74cffc49eb
yeison <yeison@yeison>
2020-11-22 17:13:42 -0500

    Pruebas unitarias

--- TallerColaborativo/src/test/Test.java
+++ TallerColaborativo/src/test/Test.java
@@ -2,11 +2,36 @@ package test;
 
 import static org.junit.Assert.*;
 
-public class Test {
+import model.Management;
 
+public class Test {
+	private Management management;
+	public Test() {
+		management = new Management("sogamoso ciudad del sol y del acero");
+	}
 	@org.junit.Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOwnName() {
+		assertEquals("Sogamoso Ciudad Del Sol y Del Acero", management.ownName());
+	}
+	
+	@org.junit.Test
+	public void testEncrypt() {
+		assertEquals("hi", management.encrypt("ab"));
+	}
+	
+	@org.junit.Test
+	public void testDecrypt() {
+		assertEquals("ab", management.decrypt("hi"));
+	}
+	
+	@org.junit.Test
+	public void testIntersection() {
+		assertEquals("colmia", management.intersection("colombia"));
+	}
+	
+	@org.junit.Test
+	public void testDifference() {
+		assertEquals("sgs udd de s y de er", management.difference("colombia"));
 	}
 
 }
